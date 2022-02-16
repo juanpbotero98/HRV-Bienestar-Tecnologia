5b800fae17c373def0957bf1535cf2f8a06ad2fb
juanpbotero98 <jp.botero@uniandes.edu.co>
2022-02-16 14:20:11 -0500

    Added BioFeedback Demo

--- Demos/OSC_Simulation.py
+++ Demos/OSC_Simulation.py
@@ -1,14 +1,15 @@
 import argparse
 import time
 from pythonosc import udp_client
+from random import randint
 
 if __name__ == "__main__":
     # Argument Parser
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type = str, default="192.168.0.3", help="ip address of the server")
     parser.add_argument("--port", type=int, default=8000, help="The port the OSC server is listening on")
-    parser.add_argument("--VideoActivation", type = int, default= 0, help= "HRV activation in the video section")
-    parser.add_argument("--SoundActivation", type = int, default= 0, help= "HRV activation in the video section")
+    parser.add_argument("--time",type=int,default=240,help="Duration of the Demo")
+    parser.add_argument("--interval",type=float,default=1.0,help="Interval in which the data is being sent in seconds")
     args = parser.parse_args()
 
     # OSC Setup
@@ -21,10 +22,10 @@ if __name__ == "__main__":
     section_times = [10,240,240,240,240] 
     section_names = ["Pre-Start","Olfative","Video","Sound","Interactive"]
     # Main Loop
-    for i in range(len(section_times)): 
-        print(section_names[i])
-        init_time = time.time()
-        while time.time()-init_time<section_times[i]:
-            client.send_message("/Start", start[i])
-            client.send_message("/HRV", hrv[i])
-            time.sleep(1)
+    init_time = time.time()
+    while time.time()-init_time<args.time:
+        HR = randint(80,90)
+        IBI = randint(800,1000)
+        client.send_message("/HR", HR)
+        client.send_message("/IBI", IBI)
+        time.sleep(args.interval)
