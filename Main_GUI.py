e2c8820f7743e93974de3c1fb14dccbd0b7ec43c
juanpbotero98 <jp.botero@uniandes.edu.co>
2022-03-25 14:05:00 -0500

    Added preliminar audio control

--- Main_GUI.py
+++ Main_GUI.py
@@ -477,10 +477,11 @@ class GUI:
 
         elif self.baseline_done:
             print('start')
-            
+
             # Randomize the color pallet of the biofeedback
             if self.OSC_transmit:
                 self.osc_utils.custom_transmit("COLOR",random.choice(self.feedback_color))
+                self.osc_utils.custom_transmit("/Audio",1)
 
             self.start_status = 1
             asyncio.run(self.main_acquisition(loop = 4,transmit=self.OSC_transmit,section_time=self.section_time))
@@ -502,6 +503,14 @@ class GUI:
 
         elif self.testmode:
             print('running test without baseline')
+            
+            # Randomize the color pallet of the biofeedback
+            if self.OSC_transmit:
+                print('Heart color sent')
+                final_time = time.time()
+                self.osc_utils.custom_transmit("/COLOR",random.choice(self.feedback_color))
+                self.osc_utils.custom_transmit("/Audio",0)
+            
             self.start_status = 0
             asyncio.run(self.main_acquisition(loop = 4,transmit=self.OSC_transmit,section_time=self.section_time))
             final_time = time.time()
@@ -538,6 +547,7 @@ class GUI:
             if self.OSC_transmit:
                 while time.time() - final_time < 15:
                     self.osc_utils.transmit(0,0,0,0)
+
         elif self.baseline_done and self.final_done:
             print("Empezó una nueva medición y se reiniciaron las variables")
             self.restart_vars()
@@ -580,7 +590,11 @@ class GUI:
     def Save_BT_command(self):
         # self.hrv_utils.Save_ECG(self.general_ecg,self.final_done,self.full_export_path)
         # self.ecg_saved = True
-        self.gui_utils.error_popup('Botón sin funcionalidad...')
+        # self.gui_utils.error_popup('Botón sin funcionalidad...')
+        if self.OSC_connected:
+            self.osc_utils.custom_transmit("/Audio",1)
+        else:
+            self.gui_utils.error_popup('No está conectado a OSC')
 
     def ExportHRV_BT_command(self):
         try:
@@ -642,6 +656,7 @@ class GUI:
     def OSC_Connect_BT_command(self):
         self.osc_utils = OSC_CommUtils(ip=self.IP_txtBox.get())
         print(" OSC connected to ip: {0}".format(self.IP_txtBox.get()))
+        self.OSC_connected = True
 
     def Mode_Popup_BT_command(self):
         self.OPmodes_popup = True
